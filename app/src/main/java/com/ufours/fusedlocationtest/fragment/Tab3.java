e45dce7bbf25caf29a9f2639978789f0bb21e7b3
ufours-24 <raandroid91@gmail.com>
2019-02-08 13:20:23 +0530

    TabBage

---
+++ app/src/main/java/com/ufours/fusedlocationtest/fragment/Tab3.java
@@ -0,0 +1,21 @@
+package com.ufours.fusedlocationtest.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ufours.fusedlocationtest.R;
+
+public class Tab3 extends Fragment {
+
+    //Overriden method onCreateView
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return inflater.inflate(R.layout.item_tab, container, false);
+    }
+}
---
+++ app/src/main/java/com/ufours/fusedlocationtest/tab_view/Activity_Tab.java
@@ -0,0 +1,81 @@
+package com.ufours.fusedlocationtest.tab_view;
+
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.readystatesoftware.viewbadger.BadgeView;
+import com.ufours.fusedlocationtest.R;
+import com.ufours.fusedlocationtest.adapter.ViewPagerAdapter;
+
+public class Activity_Tab extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    //This is our tablayout
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+
+        //Adding toolbar to the activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_delete_white_24dp));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
+
+
+        //  View mainTab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(2);
+
+
+        TextView tv = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0)).getChildAt(1));
+        BadgeView badge = new BadgeView(this, tv);
+        badge.setText("1");
+        badge.setBadgePosition(BadgeView.POSITION_CENTER);
+        badge.show();
+        // badge.increment(2);
+        badge.hide();
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+}
