a1cb789a0c99a8290a4f0fae9efa85d82f9ec991
ufours-24 <raandroid91@gmail.com>
2019-02-15 13:02:28 +0530

    Firebase Update Delete

--- app/src/main/java/com/ufours/fusedlocationtest/ActivityPagination.java
+++ app/src/main/java/com/ufours/fusedlocationtest/ActivityPagination.java
@@ -1,16 +1,19 @@
 package com.ufours.fusedlocationtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.ufours.fusedlocationtest.adapter.PaginationAdapter;
 import com.ufours.fusedlocationtest.model.DataModel;
@@ -26,19 +29,19 @@ public class ActivityPagination extends AppCompatActivity {
     private PaginationAdapter adapter;
 
     private DatabaseReference FirebaseRefernce;
-    private FirebaseDatabase  FirebaseInstance;
+    private FirebaseDatabase FirebaseInstance;
     LinearLayoutManager layoutManager;
     private boolean IsLoading = true;
     private int StartIndex = 0, EndIndex = 11, loadValue = 1;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagination);
         Rv_loadView = findViewById(R.id.Rv_loadView);
 
-         layoutManager = new LinearLayoutManager(getApplicationContext());
-         Rv_loadView.setLayoutManager(layoutManager);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        Rv_loadView.setLayoutManager(layoutManager);
 
 
         FirebaseInstance = FirebaseDatabase.getInstance();
@@ -49,7 +52,7 @@ public class ActivityPagination extends AppCompatActivity {
         loadData();
 
 
-        Rv_loadView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        /*Rv_loadView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
 
@@ -75,11 +78,51 @@ public class ActivityPagination extends AppCompatActivity {
                 }
 
             }
+        });*/
+
+
+        Rv_loadView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            GestureDetector gestureDetector = new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+            });
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if (child != null && gestureDetector.onTouchEvent(e)) {
+                    int position = rv.getChildAdapterPosition(child);
+
+
+                    String id = dataList.get(position).getCode();
+                    String name = dataList.get(position).getName();
+
+                    Intent goUpdate = new Intent(ActivityPagination.this, Activity_Data_Addd.class);
+                    goUpdate.putExtra("id",id);
+                    goUpdate.putExtra("name",name);
+                    startActivity(goUpdate);
+
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
         });
-
-
-
-
 
 
     }
@@ -106,14 +149,11 @@ public class ActivityPagination extends AppCompatActivity {
                 //creating adapter
 
 
-                if (loadValue==1)
-                {
+                if (loadValue == 1) {
                     adapter = new PaginationAdapter(ActivityPagination.this, dataList);
                     Rv_loadView.setAdapter(adapter);
-                }
-                else if (loadValue==2)
-                {
-                    IsLoading=true;
+                } else if (loadValue == 2) {
+                    IsLoading = true;
                     adapter.notifyDataSetChanged();
                 }
 
--- app/src/main/java/com/ufours/fusedlocationtest/Activity_Data_Addd.java
+++ app/src/main/java/com/ufours/fusedlocationtest/Activity_Data_Addd.java
@@ -20,8 +20,7 @@ import com.ufours.fusedlocationtest.model.DataModel;
 import com.ufours.fusedlocationtest.service.Constants;
 import com.ufours.fusedlocationtest.service.MyNotificationManager;
 
-public class Activity_Data_Addd extends AppCompatActivity
-{
+public class Activity_Data_Addd extends AppCompatActivity {
 
     private EditText Et_name;
 
@@ -30,16 +29,19 @@ public class Activity_Data_Addd extends AppCompatActivity
     private Button Bt_view;
     private DatabaseReference FirebaseRefernce;
     private FirebaseDatabase FirebaseInstance;
+
+    private String id, name;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_add);
 
-        Et_name=findViewById(R.id.Et_name);
+        Et_name = findViewById(R.id.Et_name);
 
-        Bt_submit=findViewById(R.id.Bt_submit);
+        Bt_submit = findViewById(R.id.Bt_submit);
 
-        Bt_view=findViewById(R.id.Bt_view);
+        Bt_view = findViewById(R.id.Bt_view);
 
 
         FirebaseInstance = FirebaseDatabase.getInstance();
@@ -47,31 +49,62 @@ public class Activity_Data_Addd extends AppCompatActivity
         // get reference to 'users' node
         FirebaseRefernce = FirebaseInstance.getReference();
 
+
+        id = getIntent().getStringExtra("id");
+        name = getIntent().getStringExtra("name");
+
+        if (id != null && id.length() > 0) {
+            Bt_submit.setText("Update");
+
+            Et_name.setText(name);
+        }
+
+
         Bt_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-              String Name=  Et_name.getText().toString().trim();
+                String Name = Et_name.getText().toString().trim();
 
 
-              if (Name!=null&&Name.length()>0)
-              {
-                  DataModel data=new DataModel();
-                  String id = FirebaseRefernce.push().getKey();
-                  data.setCode(id);
-                  data.setName(Name);
+                if (Name != null && Name.length() > 0) {
 
-                  FirebaseRefernce.child(id).setValue(data);
 
-                  Toast.makeText(Activity_Data_Addd.this, "Name Added Sucessfully", Toast.LENGTH_SHORT).show();
+                    if (id != null && id.length() > 0) {
+                        FirebaseRefernce = FirebaseRefernce.child(id);
 
-                  Et_name.getText().clear();
+                        //updating
+                        DataModel data = new DataModel();
+                        data.setCode(id);
+                        data.setName(Name);
+                        FirebaseRefernce.setValue(data);
+                        Toast.makeText(getApplicationContext(), "Name Updated", Toast.LENGTH_LONG).show();
 
-              }
-              else
-              {
-                  Toast.makeText(Activity_Data_Addd.this, "Invalid Name", Toast.LENGTH_SHORT).show();
-              }
+                       /* //Delete
+
+                        FirebaseRefernce.removeValue();
+
+                        Toast.makeText(Activity_Data_Addd.this, "Name Removed Sucessfully", Toast.LENGTH_SHORT).show();
+*/
+
+                    } else {
+                        DataModel data = new DataModel();
+                        String id = FirebaseRefernce.push().getKey();
+                        data.setCode(id);
+                        data.setName(Name);
+
+                        FirebaseRefernce.child(id).setValue(data);
+
+                        Toast.makeText(Activity_Data_Addd.this, "Name Added Sucessfully", Toast.LENGTH_SHORT).show();
+
+                        Et_name.getText().clear();
+
+                    }
+
+
+                } else {
+                    Toast.makeText(Activity_Data_Addd.this, "Invalid Name", Toast.LENGTH_SHORT).show();
+                }
 
 
             }
@@ -81,11 +114,10 @@ public class Activity_Data_Addd extends AppCompatActivity
             @Override
             public void onClick(View v) {
 
-                Intent go=new Intent(Activity_Data_Addd.this,ActivityPagination.class);
+                Intent go = new Intent(Activity_Data_Addd.this, ActivityPagination.class);
                 startActivity(go);
             }
         });
-
 
 
         FirebaseMessaging.getInstance().subscribeToTopic("Aram");
